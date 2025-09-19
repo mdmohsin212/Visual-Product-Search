db7f3ef1ec9aae8c18e2effd4641cf1ad0b0f990
md mohsin <siam.mohsin2005@gmail.com>
2025-09-19 16:29:13 +0600

    Optimize prediction pipeline

--- visual_product_search/pipeline/prediction_pipeline.py
+++ visual_product_search/pipeline/prediction_pipeline.py
@@ -1,4 +1,3 @@
-import torch
 import sys
 import environ
 from pathlib import Path
@@ -11,13 +10,52 @@ from visual_product_search.logger import logging
 from visual_product_search.exception import ExceptionHandle
 
 class ProductPredictionPipeline:
+    _model = None
+    _processor = None
+    _device = None
+    _Database = None
     def __init__(self, config_path="config/model.yaml"):
         try:
             self.env = environ.Env()
             environ.Env.read_env(Path(__file__).resolve().parent.parent / ".env")
             self.config = load_config(config_path)
-            self.model, self.processor, self.device = load_model(self.config["mode"]["new_model"])
-        
+            
+            if ProductPredictionPipeline._Database is None:
+                ProductPredictionPipeline._Database = DatabaseSearch(
+                    collection_name=self.env("COLLECTION_NAME")
+                )
+
         except Exception as e:
             logging.critical("Failed to load config, environment or model")
+            raise ExceptionHandle(e, sys)
+    
+    def _load_model(self):
+        if ProductPredictionPipeline._model is None:
+            ProductPredictionPipeline._model, ProductPredictionPipeline._processor, ProductPredictionPipeline._device = load_model(self.config["mode"]["new_model"])
+            logging.info("Model loaded successfully.")
+    
+    def search_with_text(self, text : str, k=5):
+        try:
+            self._load_model()
+            logging.info("Start searching using text")
+            embd = get_text_embedding(text, ProductPredictionPipeline._model, ProductPredictionPipeline._processor, ProductPredictionPipeline._device)
+            results = ProductPredictionPipeline._Database.search(embd, k)
+            logging.info("successfully find items from database")
+            return results
+        
+        except Exception as e:
+            logging.error("Searching Failed")
+            raise ExceptionHandle(e, sys)
+    
+    def search_with_image(self, image_path, k=5):
+        try:
+            self._load_model()
+            logging.info("Start searching using Image")
+            embd = get_image_embedding(image_path, ProductPredictionPipeline._model, ProductPredictionPipeline._processor, ProductPredictionPipeline._device)
+            results = ProductPredictionPipeline._Database.search(embd, k)
+            logging.info("successfully find items from database")
+            return results
+
+        except Exception as e:
+            logging.error("Searching Failed")
             raise ExceptionHandle(e, sys)
