1eb064bffe955f98cb52f52bccb5062205007d78
md mohsin <siam.mohsin2005@gmail.com>
2026-03-14 20:58:07 +0600

    Fix major error

--- visual_product_search/embeddings/embed.py
+++ visual_product_search/embeddings/embed.py
@@ -11,7 +11,7 @@ def get_text_embedding(model : CLIPModel, processor : CLIPProcessor, text, devic
         inputs = processor(text=[text], return_tensors="pt", padding=True).to(device)
 
         with torch.no_grad():
-            txt_emb = model.get_text_features(**inputs)
+            txt_emb = model.get_text_features(**inputs, return_dict=False)[0]
             txt_emb = torch.nn.functional.normalize(txt_emb, dim=-1)
             
         logging.debug(f"Image embedding generated for {text}")
@@ -28,7 +28,7 @@ def get_image_embedding(model : CLIPModel, processor : CLIPProcessor, image_path
         inputs = processor(images=image, return_tensors="pt").to(device)
 
         with torch.no_grad():
-            img_emb = model.get_image_features(**inputs)
+            img_emb = model.get_image_features(**inputs, return_dict=False)[0]
             img_emb = torch.nn.functional.normalize(img_emb, dim=-1)
 
         return img_emb.cpu().numpy().tolist()[0]
