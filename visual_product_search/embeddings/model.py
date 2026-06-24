f097e61e99204c79905f912c607da21d2965f22e
md mohsin <siam.mohsin2005@gmail.com>
2026-06-24 11:05:58 +0600

    fix error

--- visual_product_search/embeddings/model.py
+++ visual_product_search/embeddings/model.py
@@ -24,9 +24,11 @@ def load_model_for_prediction(base_model_name : str, lora_model : str, device : 
         device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"Loading CLIP model {base_model_name} on {device}")
         
-        base_model = CLIPModel.from_pretrained(base_model_name)
-        model = PeftModel.from_pretrained(base_model, lora_model, device_map={"", device})
-        processor = CLIPProcessor.from_pretrained(lora_model)
+        base_model = CLIPModel.from_pretrained(base_model_name, low_cpu_mem_usage=False)
+        model = PeftModel.from_pretrained(base_model, lora_model)
+        model.to(device)
+        
+        processor = CLIPProcessor.from_pretrained(base_model_name)
         print("Model and processor loaded successfully")
         model.eval()
         print("Model Evaluation Successfull")
